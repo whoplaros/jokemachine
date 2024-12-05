e7cb4e02cbcbcd05198cb4d4eab25fafb584bdb5
Whoplaros <whoplaros@gmail.com>
2024-12-05 08:37:15 +0200

    testing then resetting script commands

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function showAnswer(response) {
 let apiKey = "e546fd9teb385774339o4d3b5b79c3a0";
 let context = "politely provide a brief answer";
 let prompt = "Who was the first female president";
-let apiUrl =
-	"https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={apiKey}";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showAnswer);
