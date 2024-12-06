d33d70536f6e38829fab036ce50812667702cab6
Whoplaros <whoplaros@gmail.com>
2024-12-06 09:01:30 +0200

    adding button and joke
    called typewriter plugin from github and AI api from SheCodes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function showAnswer(response) {
-	alert(response.data.answer);
+	let joke = document.querySelector("#joke");
+	joke.innerHTML = response.data.answer;
+	new Typewriter("#joke", {
+		strings: response.data.answer,
+		autoStart: true,
+		delay: 10,
+	});
+}
+function tellJoke(event) {
+	event.preventDefault();
+	let apiKey = "e546fd9teb385774339o4d3b5b79c3a0";
+	let prompt = "Please tell me a joke";
+	let context =
+		"These jokes should be short and aimed at people under 12 years old";
+	let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+	let joke = document.querySelector("#joke");
+	joke.innerHTML = "Please wait while we find you a doozy...";
+	axios.get(apiURL).then(showAnswer);
 }
 
-let apiKey = "e546fd9teb385774339o4d3b5b79c3a0";
-let context = "politely provide a brief answer";
-let prompt = "Who was the first female president";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-axios.get(apiUrl).then(showAnswer);
+let button = document.querySelector("#button");
+button.addEventListener("click", tellJoke);
