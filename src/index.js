6e6543534e661c2f4feab3856023ad843bb095e1
Whoplaros <whoplaros@gmail.com>
2024-12-09 09:21:09 +0200

    css styling

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function tellJoke(event) {
 	event.preventDefault();
 	let apiKey = "e546fd9teb385774339o4d3b5b79c3a0";
 	let prompt =
-		"Please tell me a unique joke. Please provide your answer in HTML format but without including the word html or any extra unnecessary punctuation. Please also include a random funny emoji. Example as in the following example: <p>Joke question here</p><p>Joke answer here</p><p>🤣</p>";
+		"Please tell me a unique joke. Please provide your answer in HTML format but without including the word html or any extra unnecessary punctuation. Please also include two emojis. Example as in the following example: <p>Joke question here</p><p>Joke answer here</p><p>🤣</p>";
 	let context =
 		"These jokes should be short and aimed at people under 12 years old";
 	let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
